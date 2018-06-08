804296ce0d64732f346ba45a5cacb13aed95698d
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-06-08 09:04:28 +0000

    dist: added auto-generated JS distribution files

---
+++ dist/Tesseract.js
@@ -0,0 +1,13 @@
+﻿"use strict";
+/*!
+ * @file The starting point of Tesseract, exposing the framework.
+ * @author Sankarsan Kampa (a.k.a. k3rn31p4nic)
+ * @license GPL-3.0
+ * @copyright 2018 - The Bastion Bot Project
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const Constants = require("./utils/Constants");
+exports.Constants = Constants;
+const TesseractClient_1 = require("./structures/TesseractClient");
+exports.Client = TesseractClient_1.default;
+//# sourceMappingURL=Tesseract.js.map
---
+++ dist/structures/TesseractClient.js
@@ -0,0 +1,45 @@
+﻿"use strict";
+/*!
+ * @file TesseractClient Class
+ * @author Sankarsan Kampa (a.k.a. k3rn31p4nic)
+ * @license GPL-3.0
+ * @copyright 2018 - The Bastion Bot Project
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const discord_js_1 = require("discord.js");
+class TesseractClient extends discord_js_1.Client {
+    constructor(options) {
+        let isObject = typeof options === "object";
+        let hasConfigurations = 'configurations' in options;
+        let hasCredentials = 'credentials' in options;
+        let isValid = options.configurations.prefix && options.credentials.token;
+        if (!isObject || !hasConfigurations || !hasCredentials || !isValid) {
+            throw new TypeError("An invalid TesseractOptions object was passed.");
+        }
+        super(options);
+        this.options = options;
+        this.configurations = options.configurations;
+        this.credentials = options.credentials;
+    }
+    reloadSettings(configurations, credentials) {
+        if (!configurations && !credentials) {
+            return false;
+        }
+        if (configurations)
+            this.configurations = configurations;
+        if (credentials)
+            this.credentials = credentials;
+        return true;
+    }
+    login(token) {
+        if (token) {
+            this.credentials.token = token;
+        }
+        return super.login(this.credentials.token);
+    }
+    toString() {
+        return "Tesseract";
+    }
+}
+exports.default = TesseractClient;
+//# sourceMappingURL=TesseractClient.js.map
