8ec97dd61ec76bce581ee5a83bffe3cfc195a330
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-06-08 08:51:24 +0000

    src: add main file; the starting point of Tesseract

---
+++ src/Tesseract.ts
@@ -0,0 +1,14 @@
+/*!
+ * @file The starting point of Tesseract, exposing the framework.
+ * @author Sankarsan Kampa (a.k.a. k3rn31p4nic)
+ * @license GPL-3.0
+ * @copyright 2018 - The Bastion Bot Project
+ */
+
+import * as Constants from "./utils/Constants";
+import TesseractClient from "./structures/TesseractClient";
+
+export {
+  TesseractClient as Client,
+  Constants
+};
