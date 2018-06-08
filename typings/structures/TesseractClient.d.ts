293e7b295aef6dc6e589faff5f23ebd809719683
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-06-08 09:02:16 +0000

    typescript: added TypeScript typings

---
+++ typings/structures/TesseractClient.d.ts
@@ -0,0 +1,27 @@
+﻿/*!
+ * @file TesseractClient Class
+ * @author Sankarsan Kampa (a.k.a. k3rn31p4nic)
+ * @license GPL-3.0
+ * @copyright 2018 - The Bastion Bot Project
+ */
+import { Client, ClientOptions } from "discord.js";
+declare class TesseractClient extends Client {
+    configurations: ClientConfigurations;
+    credentials: ClientCredentials;
+    constructor(options: TesseractOptions);
+    reloadSettings(configurations?: ClientConfigurations, credentials?: ClientCredentials): boolean;
+    login(token?: string): Promise<string>;
+    toString(): string;
+}
+interface TesseractOptions extends ClientOptions {
+    configurations: ClientConfigurations;
+    credentials: ClientCredentials;
+}
+interface ClientConfigurations extends Object {
+    prefix: string | string[];
+}
+interface ClientCredentials extends Object {
+    token: string;
+}
+export default TesseractClient;
+//# sourceMappingURL=TesseractClient.d.ts.map
